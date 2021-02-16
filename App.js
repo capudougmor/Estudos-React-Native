e443960cb282c51783360bef750c34a477015003
Douglas Mendes <capudougmor@gmail.com>
2021-02-16 12:02:40 -0300

    Create page Mimimi

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import { View, Text } from 'react-native'
 import { AppLoading } from 'expo'
+
+import Mimimi from './src/pages/Mimimi'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Mimimi></Mimimi>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Mimimi/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import { Container } from './styles'
+
+function Mimimi() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Mimimi
