39933d5332575bbb522bf53dabdbaad850b691ca
Douglas Mendes <capudougmor@gmail.com>
2021-02-18 14:22:05 -0300

    Implemented interface mimimi

--- src/pages/Mimimi/index.jsx
+++ src/pages/Mimimi/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { Image } from 'react-native'
 
+import { Body, InputArea, Title, Input, Area, Text1,Text2 } from './styles'
 
-import { Container, Title, Input } from './styles'
+import Menino from '../images/mimimi.jpg'
 
 function Mimimi() {
   return (
-    <Container>
+    <Body>
       <Title>Exemple</Title>
-      <Input placeholder="Digite seu mimimi"></Input>
-    </Container>
+      <InputArea>
+        <Input placeholder="Digite seu mimimi"></Input>
+      </InputArea>
+      <Area>
+        <Text1>Texto 1</Text1>
+        <Image source={Menino} style={{ width: 300, height: 300, marginTop: -70} } />
+        <Text2>Texto 2</Text2>
+      </Area>
+    </Body>
   )
 }
 
--- src/pages/Mimimi/styles.js
+++ src/pages/Mimimi/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Body = styled.View`
   flex: 1;
   background: #8B10AE;
   padding-top: 30;
@@ -11,14 +11,38 @@ export const Container = styled.View`
 export const Title = styled.Text`
   font-size: 30;
   color: #fff;
-  `;
-  
-  export const Input = styled.TextInput`
-  font-size: 24;
-  color: #fff;
+`;
+
+export const InputArea = styled.View`
   align-self: stretch;
+`;
+
+export const Input = styled.TextInput`
+  font-size: 24;
+  color: #000;
   border-width: 1;
   border-color: #999;
   background-color: #eee;
-  height: 40;
+  height: 50;
+  border-radius: 10;
+  margin-inline: 10;
 `;
+
+export const Area = styled.View`
+  margin-top: 20;
+`
+
+export const Text1 = styled.Text`
+  font-size: 25;
+  color: white;
+  padding: 10px;
+  background-color: transparent;
+  font-weight: bold;
+  text-align: center;
+  height: 70;
+  z-index:2;
+`
+
+export const Text2 = styled(Text1)`
+  margin-top: -70;
+`
