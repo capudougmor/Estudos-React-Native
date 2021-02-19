6f9a4e81408edb1fc3d0219bf41f9222dd21aa38
Douglas Mendes Assunção <capudougmor@gmail.com>
2021-02-19 21:39:12 +0000

    Foods list in UseState

--- src/pages/UseState/index.jsx
+++ src/pages/UseState/index.jsx
@@ -4,21 +4,32 @@ import { useState } from 'react/cjs/react.development';
 
 import { Button } from './styles';
 
+const Desejo = ({comida}) => {
+    
+    return (
+        <Text>{comida}</Text>
+    )
+}
+
 function UseState() {
-
-  const [ state, setState ] = useState(100)
-
-  const add = () => 
-    setTimeout(() => {
-      setState((prevState) => prevState + 15)
-    }, 2000);
-
-  return (
-    <View>
-      <Button title={'Deposito: R$ 15,00'} onPress={add}/>
-      <Button title={'Saque: R$ 100,00'} onPress={() => setState(state - 100) }/>
-      <Text>R$ {state},00</Text>
-    </View>
+    const [ food, setFood] = useState()
+    
+    const [ state, setState ] = useState(100)
+    
+    const add = () => {
+        setTimeout(() => {
+            setState((prevState) => prevState + 15)
+        }, 2000);
+    }
+    
+    return (
+        <View>
+            <Button title={'Deposito: R$ 15,00'} onPress={add}/>
+            <Button title={'Saque: R$ 100,00'} onPress={() => setState(state - 100) }/>
+            <Text>R$ {state},00</Text>
+            <Text>Hoje voce vai comer</Text>
+            <Desejo comida="Pão"/>
+        </View>
   )
 }
 
