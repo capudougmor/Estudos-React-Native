c6fe207a66c0ae870b9634843cea99a78ef66ea3
Douglas Mendes Assunção <capudougmor@gmail.com>
2021-02-20 22:46:57 +0000

    Initial UserList in Crud

--- src/pages/Crud/Views/UserList.jsx
+++ src/pages/Crud/Views/UserList.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
+import users from '../data/users'
+import { ListItem, Avatar } from 'react-native-elements'
 
-export default function UserList() {
+export default props => {
+
+    function getUserItem({ item }) {
+    return (
+        <ListItem 
+            bottomDivider
+            onPress={() => props.navigation.navigate('UserForm')}
+        >
+            <Avatar source={{uri: item.avatarUrl}} />
+            <ListItem.Content>
+            <ListItem.Title>{item.name}</ListItem.Title>
+            <ListItem.Subtitle>{item.subtitle}</ListItem.Subtitle>
+            </ListItem.Content>
+            <ListItem.Chevron />
+        </ListItem>
+    )
+    }
+
     return (
         <View>
-            <Text>UerList</Text>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
         </View>
     )
 }
---
+++ src/pages/Crud/data/users.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email:'josil@empamil.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png'
+    },
+
+    {
+        id: 2,
+        name: 'Mariana Aguiar',
+        email:'riamarsil@empamil.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png',
+    },
+
+    {
+        id: 3,
+        name: 'Julio Fragoso',
+        email:'juliofra@empamil.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/02/05/08/30/cat-5983820_960_720.png',
+    },
+
+    {
+        id: 4,
+        name: 'Rafael Monteiro',
+        email:'rafamon@empamil.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/02/07/09/08/woman-5990537_960_720.png',
+    },
+]
