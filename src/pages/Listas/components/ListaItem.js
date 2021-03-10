2a3e3fec3400b44c063e62afdcf9dd65bfbcfae1
Douglas Mendes Assunção <capudougmor@gmail.com>
2021-03-10 19:40:11 +0000

    Local error

--- src/pages/Listas/components/ListaItem.js
+++ src/pages/Listas/components/ListaItem.js
@@ -4,7 +4,7 @@ import { Item, ItemText, ItemCheck } from '../styles'
 
 export default ({data, onPress}) => {
   return (
-    <Item onPress={onPress} underlayColor='#ccc' activeOpacity={0}>
+    <Item onPress={onPress} underlayColor='#ccc' activeOpacity={1}>
       <>
         <ItemText>{data.task} </ItemText>
         <ItemCheck done={data.done} ></ItemCheck>
