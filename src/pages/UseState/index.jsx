7e5cdcdad6b2e514468bbb835cdc13c2642fdb62
Douglas Mendes Assunção <capudougmor@gmail.com>
2021-04-17 00:47:11 +0000

    inserted useLayoutEffect in page UseState

--- src/pages/UseState/index.jsx
+++ src/pages/UseState/index.jsx
@@ -1,37 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import { Text, View } from 'react-native'
 import { Button } from './styles';
+import Button2 from '../Gerador/Button'
 
-import { useRoute } from '@react-navigation/native'
+import { useRoute, useNavigation } from '@react-navigation/native'
 
-const Desejo = ({senha}) => {
-    
-    return (
-        <Text>{senha}</Text>
-    )
+const Desejo = ({ senha }) => {
+
+  return (
+    <Text>{senha}</Text>
+  )
 }
 
 function Usestate() {
-    const [ state, setState ] = useState(100)
+  const [state, setState] = useState(100)
+  const [count, setCount] = useState(0)
 
-    const route = useRoute()
-    
-    const senha = route.params.password
-    
-    const add = () => {
-        setTimeout(() => {
-            setState((prevState) => prevState + 15)
-        }, 2000);
-    }
-    
-    return (
-        <View>
-            <Button title={'Deposito: R$ 15,00'} onPress={add}/>
-            <Button title={'Saque: R$ 100,00'} onPress={() => setState(state - 100) }/>
-            <Text>R$ {state},00</Text>
-            <Text>Hoje voce vai comer</Text>
-            <Desejo senha={senha}/>
-        </View>
+  const navigation = useNavigation()
+  const route = useRoute()
+  const senha = route.params.password
+
+  const add = () => {
+    setTimeout(() => {
+      setState((prevState) => prevState + 15)
+    }, 2000);
+  }
+  
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: count,
+    })
+  }, [count])
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: count,
+      headerRight: () => <Button title='+1' onPress={handleHeaderPlus} />
+    })
+  }, [])
+  
+  const handleHeaderPlus = () => {
+    setCount(c => c + 1)
+  }
+
+  return (
+    <View>
+      <Button title={'Deposito: R$ 15,00'} onPress={add} />
+      <Button title={'Saque: R$ 100,00'} onPress={() => setState(state - 100)} />
+      <Text>R$ {state},00</Text>
+      <Text>Hoje voce vai comer</Text>
+      <Desejo senha={senha} />
+    </View>
   )
 }
 
