7396b79ce95ba499b3e28e9c68334f9dbdf0dbf2
Douglas Mendes Assunção <capudougmor@gmail.com>
2021-04-19 13:42:30 +0000

    created input in page FacaUmBolo

--- src/pages/FacaUmBolo/index.jsx
+++ src/pages/FacaUmBolo/index.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/native'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 
 export default function FacaUmBolo() {
+
+  const [name, setName] = useState()
+  const [ingredients, setIngredients] = useState()
+
+  const handleAdd = () => {
+    let newIngredients = [...ingredients]
+    newIngredients.push()
+    setIngredients(newIngredients)
+  }
+
   return (
-    <View>
-      <Text>faca um bolo</Text>
-    </View>
+    <Container>
+      <Title>Faça um bolo</Title>
+      <Input
+        value={name}
+        onChangeText={e => setName(e)}
+        placeholder="Entre com um ingrediente"
+        onSubmitEditing={handleAdd}
+        returnKeyType ='send'
+      />
+      {/* <FlatList
+        data={ingredients}
+        renderItem={({ item, index }) => <ListIngredient />}
+      /> */}
+    </Container>
   )
 }
+
+const Container = styled.SafeAreaView`
+  background-color: #222;
+  flex: 1;
+  align-items: center;
+`
+
+const Title = styled.Text`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #fff;
+`
+
+const Input = styled.TextInput`
+  width: 90%;
+  height: 40px;
+  margin-top: 10px;
+  padding: 10px;
+  background-color: #fff;
+`
+
+const Button = styled.Button`
+  width: 100%;
+  margin: 30px 20px 30px;
+`
+
+const ListIngredient = styled.Text`
+  margin-top: 10px;
+`
